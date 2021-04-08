e48a1fb17f039f44070b5b3cc0ebb289099ac7db
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-08 13:49:26 +0000

    Hide Auth0 credentials

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,17 +1,17 @@
-<script lang="ts">
+<script>
   import { Auth0Context } from '@dopry/svelte-auth0'
   import { Router } from '@roxi/routify'
   import Window from '$/components/Window.svelte'
   import { routes } from '../.routify/routes'
 </script>
 
+<Window />
+
 <Auth0Context
-  domain="eseuri-prod.eu.auth0.com"
-  client_id="NJgHdaXoixiNUpEks7b3TtYi95ZH433P"
-  audience="https://eseuri-prod.eu.auth0.com/api/v2/"
+  domain={import.meta.env.VITE_AUTH0_DOMAIN}
+  client_id={import.meta.env.VITE_AUTH0_CLIENT_ID}
+  audience={import.meta.env.VITE_AUTH0_AUDIENCE}
   callback_url={window.location.origin}
   logout_url={window.location.origin}>
   <Router {routes} />
-
-  <Window />
 </Auth0Context>
