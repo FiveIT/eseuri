123e32d1c9a364bfac6b6ac36f920cf34a4fbbb2
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-08 18:02:18 +0000

    Use own package with TS definitions for Svelte Auth0 integration

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,17 +1,18 @@
-<script>
-  import { Auth0Context } from '@dopry/svelte-auth0'
+<script lang="ts">
+  import { Auth0Context } from '@tmaxmax/svelte-auth0'
   import { Router } from '@roxi/routify'
   import Window from '$/components/Window.svelte'
   import { routes } from '../.routify/routes'
+
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN as string
+  const client_id = import.meta.env.VITE_AUTH0_CLIENT_ID as string
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE as string
+  const callback_url = window.location.origin
+  const logout_url = window.location.origin
 </script>
 
 <Window />
 
-<Auth0Context
-  domain={import.meta.env.VITE_AUTH0_DOMAIN}
-  client_id={import.meta.env.VITE_AUTH0_CLIENT_ID}
-  audience={import.meta.env.VITE_AUTH0_AUDIENCE}
-  callback_url={window.location.origin}
-  logout_url={window.location.origin}>
+<Auth0Context {domain} {client_id} {audience} {callback_url} {logout_url}>
   <Router {routes} />
 </Auth0Context>
