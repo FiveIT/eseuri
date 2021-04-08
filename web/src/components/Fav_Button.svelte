647ef705a7492241eba6659878556f67d3c927a8
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-08 13:29:32 +0000

    Use alias for relative paths

--- web/src/components/Fav_Button.svelte
+++ web/src/components/Fav_Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { getContext } from 'svelte'
-  import type { Context } from '../pages/essays/[page_name].svelte'
+  import type { Context } from '$/pages/essays/[page_name].svelte'
   import Bookmark from 'svelte-material-icons/Bookmark.svelte'
   import BookmarkOutline from 'svelte-material-icons/BookmarkOutline.svelte'
-  import { contextKey } from '../pages/essays/[page_name].svelte'
+  import { contextKey } from '$/pages/essays/[page_name].svelte'
 
   const ctx = getContext<Context>(contextKey)
   console.log(ctx)
@@ -13,8 +13,7 @@
 
 <button
   class="align-middle"
-  on:click={() => ($ctx.saved ? ctx.unsave() : ctx.save())}
->
+  on:click={() => ($ctx.saved ? ctx.unsave() : ctx.save())}>
   {#if $ctx.saved}
     <Bookmark width={size} height={size} />
   {:else}
