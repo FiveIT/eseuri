f85a17a52d43328d53de08361b76e72b8960fc03
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-08 19:39:30 +0000

    Create sample test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Home', () => {
-  it('should have "Cat Facts" as title', () => {
-    cy.visit('/').get('h1').contains('Cat facts')
+  it('should have "Eseuri." as title', () => {
+    cy.visit('/').get('h1').contains('Eseuri.')
   })
 })
--- web/src/components/logo.svelte
+++ web/src/components/logo.svelte
@@ -2,9 +2,9 @@
   export let white = false
 </script>
 
-<div
+<h1
   class="text-lg font-serif font-bold subpixel-antialiased"
   class:text-white={white}
   class:text-black={!white}>
   Eseuri<span class="text-orange">.</span>
-</div>
+</h1>
