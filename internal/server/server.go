692e433b775810210343a090b9fb734e10851ec8
Matei <matei.tudose.clasa@gmail.com>
2021-04-13 16:04:05 +0000

    add upload+tika func

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,21 +1,25 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/FiveIT/template/internal/meta"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/google/go-tika/tika"
 )
 
 func New() *fiber.App {
 	app := fiber.New()
+	client := tika.NewClient(nil, meta.TikaURL)
 
 	var routes fiber.Router = app
 	if meta.IsNetlify {
 		routes = app.Group(meta.FunctionsBasePath)
 	}
 
+	//nolint:exhaustivestruct
 	routes.Use(cors.New(cors.Config{
 		AllowOrigins: meta.URL(),
 	}))
@@ -23,6 +27,23 @@ func New() *fiber.App {
 	// Routes go here
 	routes.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("sarmale cu ghimbir")
+	})
+
+	routes.Post("/upload-file", func(context *fiber.Ctx) error {
+		fisierraw, err := context.FormFile("document")
+		if err != nil {
+			return fmt.Errorf("error: %w", err)
+		}
+		fisierprocesat, err := fisierraw.Open()
+		if err != nil {
+			return fmt.Errorf("error: %w", err)
+		}
+		body, err := client.Parse(context.Context(), fisierprocesat)
+		if err != nil {
+			return fmt.Errorf("error: %w", err)
+		}
+
+		return context.SendString(body)
 	})
 
 	// 404 Not found handler
