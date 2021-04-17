718d1eadd85a154dab0ded1212ea635f7ecf8041
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 06:32:57 +0000

    Implement search functionality

--- web/src/components/SearchBar.svelte
+++ web/src/components/SearchBar.svelte
@@ -1,27 +1,54 @@
 <script lang="ts">
+  import { getContext, tick } from 'svelte'
   import Search from 'svelte-material-icons/Magnify.svelte'
-  export let query: string = ''
 
-  export let isBig = false
-  export let isAtHome = false
+  import { isActive, goto } from '@roxi/routify'
+
+  import type { Context } from './Layout.svelte'
+  import { contextKey } from './Layout.svelte'
+
+  import type { WorkType } from '$/types'
+
+  const { alive } = getContext<Context>(contextKey)
+
+  export let query: string = ''
+  export let type: WorkType = 'essay'
+
+  $: isHome = $isActive('/index')
+  $: isSearch = $isActive('/search')
+
+  function doSearch(condition = true) {
+    if (isHome && condition) {
+      // eslint-disable-next-line no-unused-vars
+      $alive = false
+      tick().then(() => $goto('/search', { query, type }))
+    } else if (isSearch) {
+      $goto('/search', { query, type })
+    }
+  }
+
+  function onKeydown(ev: KeyboardEvent) {
+    doSearch(ev.code === 'Enter')
+  }
+
+  function onClick() {
+    doSearch()
+  }
+
+  $: console.log($isActive('/search'))
 </script>
 
 <div class="flex flex-row items-center">
-  <button class="my-auto h-min ml-sm">
+  <button class="my-auto h-min ml-sm" on:click={onClick}>
     <Search color="white" />
   </button>
   <input
     type="text"
-    class="font-sans text-md subpixel-antialiased my-auto bg-white bg-opacity-0 placeholder-white text-white ml-sm w-full"
-    class:text-md={isBig}
-    class:filter-shadow={isAtHome}
-    class:filter-shadow-soft={!isAtHome}
+    class="font-sans text-sm ubpixel-antialiased my-auto bg-transparent placeholder-white text-white ml-sm w-full"
+    class:text-md={isSearch}
+    class:filter-shadow={isHome}
+    class:filter-shadow-soft={isSearch}
     placeholder="Caută titluri sau personaje"
+    on:keydown={onKeydown}
     bind:value={query} />
 </div>
-
-<style>
-  input:focus::placeholder {
-    color: transparent;
-  }
-</style>
