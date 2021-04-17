08f658970b7c034c499a15786304b63ebde92316
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 06:31:33 +0000

    Use LayoutContext

--- web/src/components/Logo.svelte
+++ web/src/components/Logo.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
   import Link from './Link.svelte'
-
-  import { contextKey } from './Layout.svelte'
-  import type { Context } from './Layout.svelte'
+  import LayoutContext from './LayoutContext.svelte'
 
   import { text } from '$/theme'
-
-  import { getContext } from 'svelte'
-
-  const { theme } = getContext<Context>(contextKey)
 </script>
 
-<Link href="/">
-  <h1 class="font-serif text-lg font-bold {text[$theme]} subpixel-antialiased">
-    Eseuri<span class="text-orange">.</span>
-  </h1>
-</Link>
+<LayoutContext let:theme>
+  <Link href="/">
+    <h1 class="font-serif text-lg font-bold {text[theme]} subpixel-antialiased">
+      Eseuri<span class="text-orange">.</span>
+    </h1>
+  </Link>
+</LayoutContext>
