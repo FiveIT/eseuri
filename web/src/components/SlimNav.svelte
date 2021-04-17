c29d401f027c6238272de70704098d8f46e8061f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 06:33:07 +0000

    Fix styles

--- web/src/components/SlimNav.svelte
+++ web/src/components/SlimNav.svelte
@@ -5,17 +5,17 @@
   import UploadButton from './UploadButton.svelte'
 </script>
 
-<nav class="w-full grid grid-cols-layout auto-rows-layout gap-y-sm">
-  <div class="flex space-x-md w-full grid-span-6">
+<nav class="w-full col-span-6 row-span-2">
+  <div class="flex w-full">
     <Logo />
-    <div class="justify-self-end grid grid-flow-row gap-x-md auto-cols-layout">
-      <NavButton href="/search" hideIfDisabled={true}>Cauta</NavButton>
+    <div class="ml-auto grid grid-flow-col gap-x-md auto-cols-layout">
+      <NavButton href="/search" hideIfDisabled={true}>Caută</NavButton>
       <NavButton enable={false}>Plagiat</NavButton>
       <NavButton enable={false}>Profesori</NavButton>
       <LoginButton />
     </div>
   </div>
-  <div class="col-start-6 col-span-1">
+  <div class="ml-auto w-min mr-md">
     <UploadButton />
   </div>
 </nav>
