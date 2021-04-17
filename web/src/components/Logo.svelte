2f3130b3f6aa6a83e20c2520cbe29b5843707a79
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 07:25:33 +0000

    Add big logo

--- web/src/components/Logo.svelte
+++ web/src/components/Logo.svelte
@@ -3,11 +3,16 @@
   import LayoutContext from './LayoutContext.svelte'
 
   import { text } from '$/theme'
+
+  export let big = false
 </script>
 
 <LayoutContext let:theme>
   <Link href="/">
-    <h1 class="font-serif text-lg font-bold {text[theme]} subpixel-antialiased">
+    <h1
+      class="font-serif text-{big ? 'xl' : 'lg'} font-bold {text[
+        theme
+      ]} subpixel-antialiased">
       Eseuri<span class="text-orange">.</span>
     </h1>
   </Link>
