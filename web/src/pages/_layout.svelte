bd2cda610fe2d72afb8269c18fca4653a5654b3d
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 03:27:58 +0000

    Refactor pages

--- web/src/pages/_layout.svelte
+++ web/src/pages/_layout.svelte
@@ -4,10 +4,7 @@
   import Blue from '$/components/blob/Blue.svelte'
 </script>
 
-<div class="min-h-full">
-  <div class="fixed bg-white w-full h-full -z-1000 blur" />
-  <Orange />
-  <Red />
-  <Blue />
-  <slot />
-</div>
+<Orange />
+<Red />
+<Blue />
+<slot />
---
+++ web/src/pages/search.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import Layout from '$/components/Layout.svelte'
+  import SlimNav from '$/components/SlimNav.svelte'
+  import Search from '$/components/SearchBar.svelte'
+  import Works from '$/components/Works.svelte'
+
+  import { store as orange } from '$/components/blob/Orange.svelte'
+  import { store as red } from '$/components/blob/Red.svelte'
+  import { store as blue } from '$/components/blob/Blue.svelte'
+  import { store as window } from '$/components/Window.svelte'
+
+  import type { BlobPropsInput, WorkType } from '$/types'
+
+  import content from '$/content'
+
+  let query = ''
+  let type: WorkType = 'essay'
+
+  $: works = content.filter(
+    ({ type: t, name: n }) =>
+      t === type && n.toLowerCase().includes(query.toLowerCase())
+  )
+
+  let orangeBlobProps: BlobPropsInput = { scale: 1.8 }
+  $: orangeBlobProps = {
+    x: -orange.width * 1.4,
+    y: $window.height - orange.height,
+  }
+
+  let redBlobProps: BlobPropsInput = {
+    scale: 2,
+    x: $window.width + red.width * 0.6,
+    y: $window.height - red.height * 0.45,
+  }
+
+  let blueBlobProps: BlobPropsInput = { scale: 17 }
+  $: blueBlobProps = {
+    x: ($window.width - blue.width * 0.8) / 2,
+    y: -blue.height * 0.635 + $window.height * 0.17,
+  }
+</script>
+
+<Layout {orangeBlobProps} {redBlobProps} {blueBlobProps} theme="white">
+  <SlimNav />
+  <div class="col-start-1 row-span-1 row-start-3 col-end-4 my-auto ">
+    <Search {query} isBig={true} isAtHome={false} />
+  </div>
+  <button
+    class="bg-opacity-0 row-span-1 row-start-3 text-white col-start-4 col-span-1 text-sm filter-shadow"
+    class:underline={type === 'essay'}
+    on:click={() => (type = 'essay')}>Eseuri</button>
+  <button
+    class="bg-opacity-0 text-white row-span-1 row-start-3 col-start-5 col-span-1 text-sm filter-shadow"
+    class:underline={type === 'characterization'}
+    on:click={() => (type = 'characterization')}>Caracterizari</button>
+  <Works {works} />
+</Layout>
