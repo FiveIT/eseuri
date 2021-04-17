67d94ff1ea241379d83f4b8c93a301989bce6e25
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-17 08:55:35 +0000

    Refactor upload

--- web/src/pages/_layout.svelte
+++ web/src/pages/_layout.svelte
@@ -1,7 +1,15 @@
-<script>
+<script lang="ts">
   import Orange from '$/components/blob/Orange.svelte'
   import Red from '$/components/blob/Red.svelte'
   import Blue from '$/components/blob/Blue.svelte'
+
+  import { setContext } from 'svelte'
+  import { contextKey } from '$/pages/upload.svelte'
+  import type { Context } from '$/pages/upload.svelte'
+
+  setContext<Context>(contextKey, {
+    file: null,
+  })
 </script>
 
 <Orange />
