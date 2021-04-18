6d3b01a1d173e3e4bcee287e3873467d27a48ea0
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-18 20:02:05 +0000

    Change aliasing, organize imports and make blue blob smaller

--- web/src/pages/search.svelte
+++ web/src/pages/search.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-  import { afterPageLoad, goto, params } from '@roxi/routify'
-
-  import Layout from '$/components/Layout.svelte'
-  import SlimNav from '$/components/SlimNav.svelte'
-  import Search from '$/components/SearchBar.svelte'
-  import Works from '$/components/Works.svelte'
-
+  import { store as blue } from '$/components/blob/Blue.svelte'
   import { store as orange } from '$/components/blob/Orange.svelte'
   import { store as red } from '$/components/blob/Red.svelte'
-  import { store as blue } from '$/components/blob/Blue.svelte'
+  import Layout from '$/components/Layout.svelte'
+  import Search from '$/components/SearchBar.svelte'
+  import SlimNav from '$/components/SlimNav.svelte'
   import { store as window } from '$/components/Window.svelte'
-
-  import type { BlobPropsInput, WorkType, Work } from '$/types'
+  import Works from '$/components/Works.svelte'
+  import content, { workTypeTranslation } from '$/content'
+  import type { BlobPropsInput, Work, WorkType } from '$/types'
   import { isWorkType } from '$/types'
-
-  import content from '$/content'
-  import { workTypeTranslation } from '$/content'
+  import { afterPageLoad, goto, params } from '@roxi/routify'
 
   let query: string = $params.query || ''
   let type: WorkType = isWorkType($params.type) ? $params.type : 'essay'
@@ -55,7 +50,7 @@
   $: blueBlobProps = {
     x: ($window.width - blue.width * 0.8) / 2,
     y: -blue.height * 0.635 + $window.height * 0.17,
-    scale: 17,
+    scale: 13,
   }
 
   $afterPageLoad(() => focusInput())
@@ -75,7 +70,7 @@
   {#each workTypes as t, i}
     <button
       class="bg-opacity-0 row-span-1 row-start-3 text-white col-start-{5 +
-        i} col-span-1 text-sm filter-shadow capitalize"
+        i} col-span-1 text-sm font-sans antialiased filter-shadow capitalize"
       class:underline={type === t}
       on:click={() => ((type = t), $goto('/search', { query, type }))}
       >{workTypeTranslation.ro[t].inarticulate.plural}</button>
