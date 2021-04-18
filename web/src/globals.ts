da4076a6348a311d07e8256127c977d6a6927116
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-18 18:35:03 +0000

    Get rem size in px

--- web/src/globals.ts
+++ web/src/globals.ts
@@ -1,3 +1,5 @@
 export const TRANSITION_DURATION = 300
 
 export { cubicOut as TRANSITION_EASING } from 'svelte/easing'
+
+export const rem = parseFloat(window.getComputedStyle(document.body).fontSize)
