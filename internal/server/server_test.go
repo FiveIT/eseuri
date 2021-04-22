8513a3ba9b9c7aa390aa6a8a0a7e2220cae9248c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-22 21:52:00 +0000

    Fix usage of log.Fatal and auto-retrieve access token

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,6 +2,7 @@ package server_test
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"log"
 	"mime/multipart"
@@ -11,14 +12,24 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/FiveIT/template/internal/meta"
 	"github.com/FiveIT/template/internal/server"
 	"github.com/gofiber/fiber/v2/utils"
 )
 
-const (
-	badToken  = "laceamnevoiedeverificarisisemnaturicrypto101"
-	goodToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im1xWEJrTUY4QWlFaHFuMlBqRUoxRyJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IlgtSGFzdXJhLURlZmF1bHQtUm9sZSI6InN0dWRlbnQiLCJYLUhhc3VyYS1BbGxvd2VkLVJvbGVzIjpbImFub255bW91cyIsInN0dWRlbnQiXSwiWC1IYXN1cmEtVXNlci1JZCI6IjEifSwiaHR0cHM6Ly9lc2V1cmkuY29tIjp7Imhhc0NvbXBsZXRlZFJlZ2lzdHJhdGlvbiI6dHJ1ZX0sImlzcyI6Imh0dHBzOi8vZXNldXJpLW1hdGVpLWRldi5ldS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDc5NDczMDU2MjI2OTc1NzcxNjgiLCJhdWQiOlsiaHR0cHM6Ly9lc2V1cmktbWF0ZWktZGV2LmV1LmF1dGgwLmNvbS9hcGkvdjIvIiwiaHR0cHM6Ly9lc2V1cmktbWF0ZWktZGV2LmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MTkwNzk3NzMsImV4cCI6MTYxOTE2NjE3MywiYXpwIjoiMk5UZ095WFJacU9NRVczd2RmVXNmU3FueElhQnc0cmMiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.nRVlz305wkU5qAR9KkYxNiY1wJxVgk3-JNQBWIs77xcyPXOO35UpGbwfXZBbsAP-sLnQi7B0ARkD1HUOSMtdriGKXD9drSYYa26bhWqcCcO9zREcQADaKd9yAwIok-8oHYWn9Z0YOqv4kKKIJV1piRVzyr9owKN7hEgBd0o5bljz7vzmZpvotbvMjCxgzVnfBHnDZ2TmYxGrA-i_6C4ksP_Co7gNR4BNk_Imy5_a_sFjubbJsqHDymwtrGihHl5IZiwbROayhw8Hr2qyblCmOoay9HbEQUq-ci5dQsfinYXA8ZpzVAnofGacAbOdtDgCAl7pbF3_ZfkSYQd6e-BgSw"
-)
+//nolint:gochecknoglobals
+var token string
+
+func TestMain(m *testing.M) {
+	var err error
+	token, err = meta.Auth0.GetAuthorizationToken(context.Background())
+
+	if err != nil {
+		log.Fatalf("Couldn't get Auth0 access token: %v", err)
+	}
+
+	os.Exit(m.Run())
+}
 
 func Test200Response(t *testing.T) {
 	t.Parallel()
@@ -72,27 +83,29 @@ func TestUploadGoodFile(t *testing.T) {
 		"titlu":          "1",
 		"tipul_lucrarii": "essay",
 	}
-	request, err := newfileUploadRequest(goodToken, "https://google.com/upload", extraParams, "document", "./testOK.odt")
+	request, err := newfileUploadRequest(token, "https://google.com/upload", extraParams, "document", "./testOK.odt")
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
 	app := server.New()
 	resp, err := app.Test(request)
 
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		body := &bytes.Buffer{}
-		_, err := body.ReadFrom(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp.Body.Close()
-		// log.Println(body)
-		utils.AssertEqual(t, 200, resp.StatusCode, "Get 200")
+		t.Fatal(err)
 	}
+
+	body := &bytes.Buffer{}
+	_, err = body.ReadFrom(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp.Body.Close()
+	// log.Println(body)
+	utils.AssertEqual(t, 200, resp.StatusCode, "Get 200")
 }
 
 func TestUploadBadFile(t *testing.T) {
@@ -102,22 +115,22 @@ func TestUploadBadFile(t *testing.T) {
 		"titlu":          "1",
 		"tipul_lucrarii": "essay",
 	}
-	request, err := newfileUploadRequest(goodToken, "https://google.com/upload", extraParams, "document", "./testBAD.txt")
+	request, err := newfileUploadRequest(token, "https://google.com/upload", extraParams, "document", "./testBAD.txt")
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
 	app := server.New()
 	resp, err := app.Test(request)
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	} else {
 		body := &bytes.Buffer{}
 		_, err := body.ReadFrom(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			t.Fatal(err)
 		}
 		resp.Body.Close()
 		// log.Println(body)
@@ -132,22 +145,22 @@ func TestUploadBadOrExpiredToken(t *testing.T) {
 		"titlu":          "1",
 		"tipul_lucrarii": "essay",
 	}
-	request, err := newfileUploadRequest(badToken, "https://google.com/upload", extraParams, "document", "./testOK.odt")
+	request, err := newfileUploadRequest("lol", "https://google.com/upload", extraParams, "document", "./testOK.odt")
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
 	app := server.New()
 	resp, err := app.Test(request)
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	} else {
 		body := &bytes.Buffer{}
 		_, err := body.ReadFrom(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			t.Fatal(err)
 		}
 		resp.Body.Close()
 		// log.Println(body)
@@ -163,22 +176,22 @@ func TestUploadGoodFileAndProfessor(t *testing.T) {
 		"tipul_lucrarii": "essay",
 		"corector_cerut": "2",
 	}
-	request, err := newfileUploadRequest(goodToken, "https://google.com/upload", extraParams, "document", "./testOK2.odt")
+	request, err := newfileUploadRequest(token, "https://google.com/upload", extraParams, "document", "./testOK2.odt")
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
 	app := server.New()
 	resp, err := app.Test(request)
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	} else {
 		body := &bytes.Buffer{}
 		_, err := body.ReadFrom(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			t.Fatal(err)
 		}
 		resp.Body.Close()
 		log.Println(body)
