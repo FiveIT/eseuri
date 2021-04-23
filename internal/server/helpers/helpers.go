334b423380259702ff2e9f4f792e086badd8ce56
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-23 02:03:54 +0000

    Use context logger and set log level based on status code.

--- internal/server/helpers/helpers.go
+++ internal/server/helpers/helpers.go
@@ -1,12 +1,23 @@
 package helpers
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 )
 
 func SendError(c *fiber.Ctx, statusCode int, message string, err error) error {
-	log.Err(err).Int("Code", statusCode).Msg(message)
+	logger := c.Locals("logger").(zerolog.Logger)
+
+	var ev *zerolog.Event
+	if statusCode >= http.StatusInternalServerError {
+		ev = logger.Error()
+	} else {
+		ev = logger.Info()
+	}
+
+	ev.Err(err).Int("code", statusCode).Msg(message)
 
 	return c.Status(statusCode).JSON(&fiber.Map{
 		"error": message,
