af2f1aea33b44780214ce8b95c7c1facb0548b4b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-23 01:56:09 +0000

    Create authentication middleware

---
+++ internal/server/middleware/auth.go
@@ -0,0 +1,87 @@
+package middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/FiveIT/template/internal/meta"
+	"github.com/FiveIT/template/internal/server/helpers"
+	jwt "github.com/form3tech-oss/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/rs/zerolog"
+)
+
+type jwtCredentials struct {
+	Type string `json:"type"`
+	Key  string `json:"key"`
+}
+
+type CustomClaims struct {
+	UserID                   int
+	Role                     string
+	HasCompletedRegistration bool
+}
+
+func (c *CustomClaims) MarshalZerologObject(z *zerolog.Event) {
+	z.
+		Int("userID", c.UserID).
+		Str("role", c.Role).
+		Bool("hasCompletedRegistration", c.HasCompletedRegistration)
+}
+
+const (
+	hasuraNamespace = "https://hasura.io/jwt/claims"
+	eseuriNamespace = "https://eseuri.com"
+)
+
+//nolint:exhaustivestruct
+func GetAuthMiddleware() (func(*fiber.Ctx) error, error) {
+	var (
+		creds jwtCredentials
+		err   error
+	)
+
+	if err = json.NewDecoder(strings.NewReader(meta.HasuraJWTSecret)).Decode(&creds); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JWT credentials: %w", err)
+	}
+
+	mid := jwtware.New(jwtware.Config{
+		SigningMethod: creds.Type,
+		SigningKey:    creds.Key,
+		SuccessHandler: func(c *fiber.Ctx) error {
+			logger := c.Locals("logger").(zerolog.Logger)
+
+			user := c.Locals("user").(jwt.MapClaims)
+
+			logger.Debug().Fields(user).Msg("claims")
+
+			hasura := user[hasuraNamespace].(map[string]interface{})
+			eseuri := user[eseuriNamespace].(map[string]interface{})
+
+			claims := CustomClaims{}
+
+			claims.Role = hasura["X-Hasura-Default-Role"].(string)
+			claims.UserID, _ = strconv.Atoi(hasura["X-Hasura-User-Id"].(string))
+			claims.HasCompletedRegistration = eseuri["hasCompletedRegistration"].(bool)
+
+			logger.Debug().EmbedObject(&claims).Msg("unmarshaled custom claims")
+
+			if !claims.HasCompletedRegistration {
+				return helpers.SendError(c, http.StatusUnauthorized, "unregistered user", nil)
+			}
+
+			c.Locals("claims", claims)
+
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return helpers.SendError(c, http.StatusUnauthorized, "invalid or expired token", e)
+		},
+	})
+
+	return mid, nil
+}
