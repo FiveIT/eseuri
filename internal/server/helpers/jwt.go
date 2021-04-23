994fede39370a6332b23b60443cee49615b92afb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-23 01:55:57 +0000

    Move into auth middleware file

--- internal/server/helpers/jwt.go
+++
@@ -1,8 +0,0 @@
-package helpers
-
-const (
-	// HasuraNamespace is the namespace for Hasura's JWT custom claims.
-	HasuraNamespace = "https://hasura.io/jwt/claims"
-	// EseuriNamespace is the namespace for our platform's JWT custom claims.
-	EseuriNamespace = "https://eseuri.com"
-)
