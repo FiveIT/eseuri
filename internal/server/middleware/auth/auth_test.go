cdbdf0810c2fac9b1e6a8682ad0a097b16fa4889
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-23 10:42:25 +0000

    Remove unportable and unnecessary test cases

--- internal/server/middleware/auth/auth_test.go
+++ internal/server/middleware/auth/auth_test.go
@@ -26,7 +26,7 @@ func App(tb testing.TB, customClaims ...chan auth.CustomClaims) *fiber.App {
 		return c.Next()
 	}
 
-	return testhelper.App(tb, "hello", logger.Middleware(nil), auth.Middleware(), claimsMiddleware)
+	return testhelper.App(tb, logger.Middleware(nil), auth.Middleware(), claimsMiddleware)
 }
 
 type testCase struct {
@@ -62,27 +62,15 @@ func TestInvalidJWTs(t *testing.T) {
 	app := App(t)
 	tests := []testCase{
 		{
-			Name:               "Missing",
-			Token:              "",
-			ExpectedStatusCode: http.StatusBadRequest,
-			ExpectedResponse:   "missing or malformed token",
-		},
-		{
 			Name:               "Malformed",
 			Token:              "amSarmale",
 			ExpectedStatusCode: http.StatusBadRequest,
 			ExpectedResponse:   "missing or malformed token",
 		},
-		// TODO: Add tokens that satisfy the conditions
 		{
-			Name:               "Expired",
-			Token:              ``,
-			ExpectedStatusCode: http.StatusUnauthorized,
-			ExpectedResponse:   "invalid or expired token",
-		},
-		{
-			Name:               "Invalid",
-			Token:              ``,
+			Name: "Invalid",
+			//nolint:lll
+			Token:              `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im1xWEJrTUY4QWlFaHFuMlBqRUoxRyJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IlgtSGFzdXJhLURlZmF1bHQtUm9sZSI6InN0dWRlbnQiLCJYLUhhc3VyYS1BbGxvd2VkLVJvbGVzIjpbImFub255bW91cyIsInN0dWRlbnQiXSwiWC1IYXN1cmEtVXNlci1JZCI6IjEifSwiaHR0cHM6Ly9lc2V1cmkuY29tIjp7Imhhc0NvbXBsZXRlZFJlZ2lzdHJhdGlvbiI6dHJ1ZX0sImlzcyI6Imh0dHBzOi8vZXNldXJpLW1hdGVpLWRldi5ldS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDc5NDczMDU2MjI2OTc1NzcxNjgiLCJhdWQiOlsiaHR0cHM6Ly9lc2V1cmktbWF0ZWktZGV2LmV1LmF1dGgwLmNvbS9hcGkvdjIvIiwiaHR0cHM6Ly9lc2V1cmktbWF0ZWktZGV2LmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MTkwNzk3NzMsImV4cCI6MTYxOTE2NjE3MywiYXpwIjoiMk5UZ095WFJacU9NRVczd2RmVXNmU3FueElhQnc0cmMiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.nRVlz305wkU5qAR9KkYxNiY1wJxVgk3-JNQBWIs77xcyPXOO35UpGbwfXZBbsAP-sLnQi7B0ARkD1HUOSMtdriGKXD9drSYYa26bhWqcCcO9zREcQADaKd9yAwIok-8oHYWn9Z0YOqv4kKKIJV1piRVzyr9owKN7hEgBd0o5bljz7vzmZpvotbvMjCxgzVnfBHnDZ2TmYxGrA-i_6C4ksP_Co7gNR4BNk_Imy5_a_sFjubbJsqHDymwtrGihHl5IZiwbROayhw8Hr2qyblCmOoay9HbEQUq-ci5dQsfinYXA8ZpzVAnofGacAbOdtDgCAl7pbF3_ZfkSYQd6e-BgSw`,
 			ExpectedStatusCode: http.StatusUnauthorized,
 			ExpectedResponse:   "invalid or expired token",
 		},
@@ -105,11 +93,7 @@ func TestValidJWT(t *testing.T) {
 	res := testhelper.DoTestRequest(t, app, req)
 	defer res.Body.Close()
 
-	body := testhelper.ReadString(t, res.Body)
-
-	// TODO: Simplify success checking only to asserting the status code
-	utils.AssertEqual(t, http.StatusOK, res.StatusCode)
-	utils.AssertEqual(t, "hello", body)
+	utils.AssertEqual(t, http.StatusNoContent, res.StatusCode)
 
 	if claims := <-ch; (claims.Role != "student" && claims.Role != "teacher") || claims.UserID == 0 {
 		t.Fatalf("Invalid custom claims: %+v", claims)
