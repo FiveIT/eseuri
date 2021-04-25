2698bd9b74425c851f6b474788c7060c3455d82b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-25 18:28:24 +0000

    Use global logger when context logger is not present.
    It is unknown why this happens.

--- internal/server/helpers/helpers.go
+++ internal/server/helpers/helpers.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func SendError(c *fiber.Ctx, statusCode int, message string, err error) error {
-	logger := c.Locals("logger").(zerolog.Logger)
+	logger, ok := c.Locals("logger").(zerolog.Logger)
+	if !ok {
+		logger = log.Logger
+		logger.Err(err).Stack().Msg("context logger does not exist")
+	}
 
 	var ev *zerolog.Event
 	if statusCode >= http.StatusInternalServerError {
