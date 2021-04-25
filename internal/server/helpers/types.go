26f9da04dfcace25aa9ad13f2f0ec7a66ab19174
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-25 20:45:14 +0000

    Remove file from work input as it can't be parsed here

--- internal/server/helpers/types.go
+++ internal/server/helpers/types.go
@@ -1,10 +1,7 @@
 package helpers
 
-import "mime/multipart"
-
 type WorkFormInput struct {
-	File               *multipart.FileHeader `form:"file"`
-	Type               string                `form:"type"`
-	SubjectID          int                   `form:"subject"`
-	RequestedTeacherID int                   `form:"requestedTeacher"`
+	Type               string `form:"type"`
+	SubjectID          int    `form:"subject"`
+	RequestedTeacherID int    `form:"requestedTeacher"`
 }
