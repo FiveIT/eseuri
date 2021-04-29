e16e8420beece58ef6ea323dc06fd68fdb63c235
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-29 18:08:50 +0000

    Add innerShadow theme entry

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -28,6 +28,11 @@ export const filterShadow: ThemeEntry = {
   white: 'filter-shadow',
 }
 
+export const innerShadow: ThemeEntry = {
+  default: '',
+  white: 'shadow-inner-large',
+}
+
 export const background: ThemeEntry = {
   default: 'bg-white',
   white: 'bg-blue',
