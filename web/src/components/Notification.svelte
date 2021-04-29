32d17865490c8a66397da2829214540fea5dfe31
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-29 18:14:40 +0000

    Add shadows

--- web/src/components/Notification.svelte
+++ web/src/components/Notification.svelte
@@ -52,10 +52,20 @@
       },
     },
   }
+
+  const iconShadow: Record<Theme, string> = {
+    default: '',
+    white: 'filter-shadow-soft',
+  }
+
+  const baseShadow: Record<Theme, string> = {
+    default: 'filter-shadow-large',
+    white: 'filter-shadow',
+  }
 </script>
 
 <script lang="ts">
-  import { text, border, filterShadow, background } from '$/theme'
+  import { text, border, filterShadow, background, innerShadow } from '$/theme'
   import { getLayout } from './Layout.svelte'
   import { px } from '$/util'
   import { tick } from 'svelte'
@@ -90,11 +100,12 @@
 </script>
 
 <div
-  class="w-notification bg-white min-h-notification z-10 rounded fixed top-4/5 left-3/4 transition-all duration-50 ease-out {text[
+  class="w-notification min-h-notification z-10 rounded fixed top-4/5 left-3/4 transition-all duration-50 ease-out {text[
     theme
-  ]} {border.color[theme]} {border.size[theme]} {filterShadow[
+  ]} {border.color[theme]} {border.size[theme]} {baseShadow[
     theme
-  ]} {background[
+  ]}
+  {background[theme]} {innerShadow[
     theme
   ]} text-sm font-sans antialiased leading-none flex flex-col"
   on:mouseenter={handleMouseOver}
@@ -102,17 +113,19 @@
   bind:this={parent}
   transition:fade={{ easing, duration }}>
   <div class="flex w-full h-notification flex-row items-center px-sm">
-    <svelte:component
-      this={currentAssets.icon}
-      color="var(--{currentAssets.color})"
-      size={px(2.5)} />
-    <p class="mx-sm">
+    <div class={iconShadow[theme]}>
+      <svelte:component
+        this={currentAssets.icon}
+        color="var(--{currentAssets.color})"
+        size={px(2.5)} />
+    </div>
+    <p class="mx-sm {filterShadow[theme]}">
       {message}
     </p>
   </div>
   {#if show && explanation !== ''}
     <p
-      class="mx-sm mb-sm"
+      class="mx-sm mb-sm {filterShadow[theme]}"
       bind:offsetHeight={detailsHeight}
       transition:slide={{ easing, duration: 50 }}>
       {explanation}
