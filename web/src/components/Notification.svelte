08122fc1e98a76a77ca85ae2196074221603b31a
Julien <julieniancu@gmail.com>
2021-04-29 14:41:45 +0000

    Added transition
    Co-authored-by: Teodor Maxim <tmaxmax@users.noreply.github.com>

--- web/src/components/Notification.svelte
+++ web/src/components/Notification.svelte
@@ -1,86 +1,103 @@
-<script lang="ts">
+<script context="module" lang="ts">
+  import type { Theme } from '$/types'
   import Error from 'svelte-material-icons/AlertCircle.svelte'
   import ErrorBlue from 'svelte-material-icons/AlertCircleOutline.svelte'
   import Check from 'svelte-material-icons/CheckCircle.svelte'
   import CheckBlue from 'svelte-material-icons/CheckCircleOutline.svelte'
   import Information from 'svelte-material-icons/Information.svelte'
   import InformationBlue from 'svelte-material-icons/InformationOutline.svelte'
-  import LayoutContext from './LayoutContext.svelte'
-  import { text, border, filterShadow, background } from '$/theme'
-  import { px } from '$/util'
   import { slide } from 'svelte/transition'
-  export let type: string
+
+  interface Assets {
+    /* The icon's Svelte component */
+    icon: any
+    color: string
+  }
+
+  type Status = 'good' | 'error' | 'info'
+
+  const assets: Record<Status, Record<Theme, Assets>> = {
+    good: {
+      default: {
+        icon: Check,
+        color: 'dark-green',
+      },
+      white: {
+        icon: CheckBlue,
+        color: 'light-green',
+      },
+    },
+    error: {
+      default: {
+        icon: Error,
+        color: 'red',
+      },
+      white: {
+        icon: ErrorBlue,
+        color: 'red',
+      },
+    },
+    info: {
+      default: {
+        icon: Information,
+        color: 'grey',
+      },
+      white: {
+        icon: InformationBlue,
+        color: 'grey',
+      },
+    },
+  }
+</script>
+
+<script lang="ts">
+  import { text, border, filterShadow, background } from '$/theme'
+  import { getLayout } from './Layout.svelte'
+  import { px } from '$/util'
+
+  const { theme: themeStore } = getLayout()
+
+  export let type: Status
   export let message: string = 'Eroare'
   export let explanation: string = ''
+
   let show: boolean = false
+
   function handleMouseOver() {
     console.log('lala')
     show = true
   }
+
   function handleMouseOut() {
     show = false
   }
+
+  $: theme = $themeStore
+  $: currentAssets = assets[type][theme]
 </script>
 
-<LayoutContext let:theme>
-  <div
-    class="w-notification_width bg-white min-h-notification_height z-10 rounded fixed top-3/4 left-3/4 transition duration-50 ease-out {text[
-      theme
-    ]} {border.color[theme]} {border.size[theme]} {filterShadow[
-      theme
-    ]} {background[theme]} text-sm font-sans antialiased"
-    on:mouseenter={handleMouseOver}
-    on:mouseleave={handleMouseOut}>
-    {#if type === 'error'}
-      <div class="w-full  flex align-middle items-center flex-col ">
-        <div class="w-full h-full flex flex-row my-sm px-sm">
-          {#if background[theme] == 'bg-white'}
-            <Error color="var(--red)" size={px(2.5)} />
-          {:else}
-            <ErrorBlue color="var(--red)" size={px(2.5)} />
-          {/if}
-          <p class="mx-sm my-auto">{message}</p>
-        </div>
-        {#if show}
-          <p class="mx-sm my-auto p-sm" transition:slide|local>
-            {explanation}
-          </p>
-        {/if}
-      </div>
-    {:else if type === 'good'}
-      <div class="w-full  flex align-middle items-center flex-col">
-        <div class="w-full flex flex-row my-sm px-sm">
-          {#if background[theme] == 'bg-white'}
-            <Check color="var(--dark-green)" size={px(2.5)} />
-          {:else}
-            <CheckBlue color="var(--light-green)" size={px(2.5)} />
-          {/if}
-          <p class="mx-sm my-auto ">
-            {message}
-          </p>
-        </div>
-        {#if show}
-          <p class="mx-sm my-auto">
-            {explanation}
-          </p>
-        {/if}
-      </div>
-    {:else}
-      <div class="w-full  flex align-middle items-center flex-col">
-        <div class="w-full flex flex-row my-sm px-sm">
-          {#if background[theme] == 'bg-white'}
-            <Information color="var(--gray)" size={px(2.5)} />
-          {:else}
-            <InformationBlue color="var(--light-gray)" size={px(2.5)} />
-          {/if}
-          <p class="mx-sm my-auto">{message}</p>
-        </div>
-        {#if show}
-          <p class="mx-sm my-auto">
-            {explanation}
-          </p>
-        {/if}
-      </div>
+<div
+  class="w-notification_width bg-white min-h-notification_height z-10 rounded fixed top-3/4 left-3/4 transition duration-50 ease-out {text[
+    theme
+  ]} {border.color[theme]} {border.size[theme]} {filterShadow[
+    theme
+  ]} {background[theme]} text-sm font-sans antialiased leading-none"
+  on:mouseenter={handleMouseOver}
+  on:mouseleave={handleMouseOut}>
+  <div class="flex align-middle items-center flex-col">
+    <div class="flex w-full flex-row my-sm px-sm">
+      <svelte:component
+        this={currentAssets.icon}
+        color="var(--{currentAssets.color})"
+        size={px(2.5)} />
+      <p class="mx-sm my-auto">
+        {message}
+      </p>
+    </div>
+    {#if show}
+      <p class="mx-sm mb-sm" transition:slide|local>
+        {explanation}
+      </p>
     {/if}
   </div>
-</LayoutContext>
+</div>
