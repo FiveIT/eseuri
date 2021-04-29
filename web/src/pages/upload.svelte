a2db4e79e509d9597e999b9db1fb4d962266fc54
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-29 21:08:57 +0000

    Implement authorization check for upload

--- web/src/pages/upload.svelte
+++ web/src/pages/upload.svelte
@@ -21,6 +21,7 @@
   import { px } from '$/lib/util'
   import { goto } from '@roxi/routify'
   import { getContext } from 'svelte'
+  import Allow, { AUTHENTICATED, REGISTERED } from '$/components/Allow.svelte'
   import Docs from 'svelte-material-icons/FileDocumentBoxMultiple.svelte'
   import GoogleDocs from 'svelte-material-icons/FileDocumentOutline.svelte'
   import Doodle from 'svelte-material-icons/Gesture.svelte'
@@ -52,46 +53,48 @@
   const ctx = getContext<Context>(contextKey)
 </script>
 
-<Layout {orangeBlobProps} {redBlobProps} {blueBlobProps} center>
-  <LayoutContext let:alive>
-    <div
-      class="blur row-span-6 col-span-2 border -mt-sm -ml-md rounded white-bg bg-opacity-50 grid auto-rows-1fr grid-flow-row px-md py-sm gap-y-sm text-sm font-sans antialiased">
-      <h2 class="m-auto">Publică o lucrare</h2>
-      <UploadVariantPlaceholder icon={ScrieAici} fg="black" bg="white" border>
-        Scrie-o aici
-      </UploadVariantPlaceholder>
-      <label
-        class="flex items-center p-sm rounded bg-blue text-white justify-between cursor-pointer focus-within:outline-solid-black select-none">
-        Încarcă un document
-        <Docs color="var(--white)" size={px(1.5)} />
-        <input
-          name="file"
-          type="file"
-          accept=".txt,.doc,.docx,.odt,.rtf"
-          class="opacity-0 w-0 h-0 absolute"
-          on:change={() => {
-            if (!input.files) {
-              return
-            }
-            ctx.file = input.files[0]
-            go('/upload_configure', alive, $goto)
-          }}
-          bind:this={input} />
-      </label>
-      <UploadVariantPlaceholder icon={GoogleDocs} bg="google-docs">
-        Încarcă din Google Docs
-      </UploadVariantPlaceholder>
-      <h3 class="m-auto">Ai scris de mână?</h3>
-      <UploadVariantPlaceholder icon={Doodle} bg="red">
-        Încarcă imagini/PDF
-      </UploadVariantPlaceholder>
-    </div>
-    <div class="row-start-3 row-span-2 col-start-4 col-span-2">
-      <Logo big={true} />
-    </div>
-    <h3
-      class="row-start-5 row-span-1 col-start-4 col-end-6 text-sm font-sans text-sm antialiased">
-      Perfecționează-ți-le cu cea mai extinsă colecție din România!
-    </h3>
-  </LayoutContext>
-</Layout>
+<Allow when={AUTHENTICATED | REGISTERED} redirect="/">
+  <Layout {orangeBlobProps} {redBlobProps} {blueBlobProps} center>
+    <LayoutContext let:alive>
+      <div
+        class="blur row-span-6 col-span-2 border -mt-sm -ml-md rounded white-bg bg-opacity-50 grid auto-rows-1fr grid-flow-row px-md py-sm gap-y-sm text-sm font-sans antialiased">
+        <h2 class="m-auto">Publică o lucrare</h2>
+        <UploadVariantPlaceholder icon={ScrieAici} fg="black" bg="white" border>
+          Scrie-o aici
+        </UploadVariantPlaceholder>
+        <label
+          class="flex items-center p-sm rounded bg-blue text-white justify-between cursor-pointer focus-within:outline-solid-black select-none">
+          Încarcă un document
+          <Docs color="var(--white)" size={px(1.5)} />
+          <input
+            name="file"
+            type="file"
+            accept=".txt,.doc,.docx,.odt,.rtf"
+            class="opacity-0 w-0 h-0 absolute"
+            on:change={() => {
+              if (!input.files) {
+                return
+              }
+              ctx.file = input.files[0]
+              go('/upload_configure', alive, $goto)
+            }}
+            bind:this={input} />
+        </label>
+        <UploadVariantPlaceholder icon={GoogleDocs} bg="google-docs">
+          Încarcă din Google Docs
+        </UploadVariantPlaceholder>
+        <h3 class="m-auto">Ai scris de mână?</h3>
+        <UploadVariantPlaceholder icon={Doodle} bg="red">
+          Încarcă imagini/PDF
+        </UploadVariantPlaceholder>
+      </div>
+      <div class="row-start-3 row-span-2 col-start-4 col-span-2">
+        <Logo big={true} />
+      </div>
+      <h3
+        class="row-start-5 row-span-1 col-start-4 col-end-6 text-sm font-sans text-sm antialiased">
+        Perfecționează-ți-le cu cea mai extinsă colecție din România!
+      </h3>
+    </LayoutContext>
+  </Layout>
+</Allow>
