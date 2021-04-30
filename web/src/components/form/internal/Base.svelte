215fb8c0907e06fab43026102c4b84fd6bd12eeb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-30 21:23:58 +0000

    Add font style to base form layout

--- web/src/components/form/internal/Base.svelte
+++ web/src/components/form/internal/Base.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <LayoutContext let:theme>
-  <div class="grid gap-x-md auto-cols-layout grid-flow-col">
+  <div class="grid gap-x-md auto-cols-layout grid-flow-col text-sm font-sans">
     <slot {theme} />
   </div>
 </LayoutContext>
