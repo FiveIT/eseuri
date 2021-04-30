daa72fd9771381a53466d959e6f42c0f01c29e71
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-30 21:25:05 +0000

    Create form select component

---
+++ web/src/components/form/Select.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Base from './internal/Base.svelte'
+  import { placeholderText, text } from '$/lib/theme'
+
+  export let options: readonly any[]
+  export let mapper = (option: any) => option
+  export let display = (value: any) => value
+
+  export let name: string
+  export let placeholder = ''
+  export let required = false
+
+  export let value: any = undefined
+</script>
+
+<Base let:theme>
+  <label for={name} class="place-self-center select-none {text[theme]}">
+    <slot />
+  </label>
+  <select
+    {name}
+    id={name}
+    {placeholder}
+    {required}
+    bind:value
+    class="col-span-2 {placeholderText[theme]} {text[theme]} bg-transparent">
+    {#each options as opt}
+      <option value={mapper(opt)} class="shadow-none">{display(opt)}</option>
+    {/each}
+  </select>
+</Base>
