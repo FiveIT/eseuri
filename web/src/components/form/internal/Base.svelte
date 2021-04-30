3d3b0ebd5a0f764708c2881154aec81dcd57205f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-30 19:49:05 +0000

    Expose theme to slot

--- web/src/components/form/internal/Base.svelte
+++ web/src/components/form/internal/Base.svelte
@@ -1,3 +1,9 @@
-<div class="grid gap-x-md auto-cols-layout grid-flow-col text-sm">
-  <slot />
-</div>
+<script>
+  import LayoutContext from '../../LayoutContext.svelte'
+</script>
+
+<LayoutContext let:theme>
+  <div class="grid gap-x-md auto-cols-layout grid-flow-col">
+    <slot {theme} />
+  </div>
+</LayoutContext>
