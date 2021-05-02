0a6cae2dd6aa2b7b0c4c9b823cfdcb7dff938ec4
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-02 16:27:13 +0000

    Add will-change per MDN recommendation

--- web/src/pages/work/_/navigation/internal/Base.svelte
+++ web/src/pages/work/_/navigation/internal/Base.svelte
@@ -5,3 +5,9 @@
 <button on:click class="h-min sticky top-1/2 flex justify-center items-center">
   <svelte:component this={icon} size="4rem" color="var(--gray-light)" />
 </button>
+
+<style>
+  button {
+    will-change: transform;
+  }
+</style>
