1b621c34028984e6f8442dd0f613d6957b408ab1
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-04 08:05:34 +0000

    Fix list functions returning unapproved works

--- db/migrations/default/1618597125038_works_random_list_view/up.sql
+++ db/migrations/default/1618597125038_works_random_list_view/up.sql
@@ -2,17 +2,20 @@ set search_path to public;
 
 create domain seed as float check (value <= 1) check (value >= -1);
 
--- TODO: add join on works in order to check status
 create function list_essays(titleID int, seed seed) returns setof essays stable as $$
 begin
     perform setseed(seed);
-    return query select * from essays where title_id = titleID and status = 'approved' order by random();
+    return query select work_id, title_id from essays
+        left join works on work_id = id
+    where title_id = titleID and status = 'approved' order by random();
 end;
 $$ language plpgsql;
 
 create function list_characterizations(characterID int, seed seed) returns setof characterizations stable as $$
 begin
     perform setseed(seed);
-    return query select * from characterizations where character_id = characterID and status = 'approved' order by random();
+    return query select work_id, character_id from characterizations
+        left join works on work_id = id
+    where character_id = characterID and status = 'approved' order by random();
 end;
 $$ language plpgsql;
