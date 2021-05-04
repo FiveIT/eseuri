d9e892bcfad21e2c6a8496514bb9650adcdda116
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-04 00:39:32 +0000

    Fix list functions

--- db/migrations/default/1618597125038_works_random_list_view/up.sql
+++ db/migrations/default/1618597125038_works_random_list_view/up.sql
@@ -5,13 +5,13 @@ create domain seed as float check (value <= 1) check (value >= -1);
 create function list_essays(titleID int, seed seed) returns setof essays stable as $$
 begin
     perform setseed(seed);
-    return query select * from essays where title_id = titleID order by random();
+    return query select * from essays where title_id = titleID and status = 'approved' order by random();
 end;
 $$ language plpgsql;
 
 create function list_characterizations(characterID int, seed seed) returns setof characterizations stable as $$
 begin
     perform setseed(seed);
-    return query select * from characterizations where character_id = characterID order by random();
+    return query select * from characterizations where character_id = characterID and status = 'approved' order by random();
 end;
 $$ language plpgsql;
