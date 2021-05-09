0b5449c984251d046d02d461a91ad6dbe311b6ab
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-09 11:27:12 +0000

    Extract action layout into separate component

--- web/src/components/form/internal/ActionsBase.svelte
+++ web/src/components/form/internal/ActionsBase.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
   import Submit from '../Submit.svelte'
+  import ActionsLayout from './ActionsLayout.svelte'
   import { getForm } from '../Form.svelte'
 
-  const { rows, cols, disabled } = getForm()
+  const { disabled } = getForm()
 
   export let submitValue: string | undefined = undefined
 </script>
 
-<div class="row-start-{rows + 2} col-span-{3 * cols} flex justify-center">
-  <div class="grid grid-flow-col auto-cols-layout h-full gap-x-md">
-    {#if !$disabled}
-      <Submit value={submitValue}><slot /></Submit>
-    {/if}
-    <slot name="abort" />
-  </div>
-</div>
+<ActionsLayout>
+  {#if !$disabled}
+    <Submit value={submitValue}><slot /></Submit>
+  {/if}
+  <slot name="abort" />
+</ActionsLayout>
---
+++ web/src/components/form/internal/ActionsLayout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { getForm } from '../Form.svelte'
+
+  const { rows, cols } = getForm()
+</script>
+
+<div class="row-start-{rows + 2} col-span-{3 * cols} flex justify-center">
+  <div class="grid grid-flow-col auto-cols-layout h-full gap-x-md">
+    <slot /> 
+  </div>
+</div>
