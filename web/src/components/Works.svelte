f0bd10facc82176ed4ec84ee8ca324f46534cd17
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-09 00:08:59 +0000

    Fix no works check

--- web/src/components/Works.svelte
+++ web/src/components/Works.svelte
@@ -20,7 +20,7 @@
 <LayoutContext let:theme>
   <div
     class="grid w-full h-full grid-cols-essays auto-rows-essays gap-x-lg gap-y-sm col-start-1 col-end-7">
-    {#if !works || works.length}
+    {#if !works?.length}
       <p
         class="col-start-2 place-self-center text-center text-md font-sans antialiased {placeholderText[
           theme
