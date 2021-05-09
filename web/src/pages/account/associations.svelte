f7a3b3b6369dc56f3aca9556b012310855a9effc
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-09 19:56:47 +0000

    Extract route components in routes

--- web/src/pages/account/associations.svelte
+++ web/src/pages/account/associations.svelte
@@ -1,5 +1,22 @@
-<script>
-  import Associations from '$/components/Teachers.svelte'
+<script lang="ts">
+  import AssociateButton from '$/components/AssociateButton.svelte'
+  import AssociateCard from '$/components/AssociateCard.svelte'
+
+  import { asociates } from '$/content'
+
+  let todelete: boolean
+  $: if (todelete == true) {
+    todelete = false
+    asociates.splice(position)
+    console.log(position)
+  }
+  let position: number
 </script>
 
-<Associations />
+<div
+  class="row-start-5 col-span-6 col-start-1 grid grid-cols-essays auto-rows-essays gap-x-md gap-y-sm ">
+  <AssociateButton />
+  {#each asociates as associate, i}
+    <AssociateCard work={associate} {todelete} {i} bind:position />
+  {/each}
+</div>
--- web/src/pages/account/works.svelte
+++ web/src/pages/account/works.svelte
@@ -1,5 +1,61 @@
-<script>
-  import Lucrari from '$/components/Lucrari.svelte'
+<script lang="ts">
+  import { LayoutContext } from '$/components'
+  import { lucrari } from '$/content'
+  import { text, filterShadow } from '$/lib'
+  import LucrariModel from '$/components/LucrariModel.svelte'
+
+  type Choosen = 'InLucru' | 'InAsteptare' | 'Aprobate' | 'Respinse' | 'InRevizuire'
+
+  let selected: Choosen
+  selected = 'InLucru'
+  $: works = lucrari.filter(lucrari => lucrari.status === selected)
 </script>
 
-<Lucrari />
+<LayoutContext let:theme>
+  <div
+    class=" z-10 {text[theme]} {filterShadow[
+      theme
+    ]}   col-start-1 col-span-6 row-start-5 row-span-6 grid grid-cols-6 grid-rows-6 gap-x-md gap-y-sm mt-sm ">
+    <button
+      class:underline={selected == 'InLucru'}
+      class="col-start-1 row-start-2 w-full h-full "
+      on:click={() => (selected = 'InLucru')}>În lucru</button>
+    <button
+      class:underline={selected == 'InAsteptare'}
+      class="col-start-1 row-start-3 w-full h-full "
+      on:click={() => (selected = 'InAsteptare')}>În așteptare</button>
+    <button
+      class:underline={selected == 'InRevizuire'}
+      class="col-start-1 row-start-4 w-full h-full "
+      on:click={() => (selected = 'InRevizuire')}>În revizuire acum</button>
+    <button
+      class:underline={selected == 'Aprobate'}
+      class="col-start-1 row-start-5 w-full h-full "
+      on:click={() => (selected = 'Aprobate')}>Aprobate</button>
+    <button
+      class:underline={selected == 'Respinse'}
+      class="col-start-1 row-start-6 w-full h-full "
+      on:click={() => (selected = 'Respinse')}>Respinse</button>
+    <div class="col-start-2 row-start-1 my-auto text-center">Tip</div>
+    <div class="col-start-3 col-span-2 row-start-1 my-auto text-center">Subiect</div>
+    <div class="col-start-5 row-start-1 my-auto text-center">
+      {#if selected === 'Aprobate'}
+        Timpul<br /> Aprobarii
+      {:else if selected === 'Respinse'}
+        Timpul <br />Respingerii
+      {:else}
+        Ultima <br />Actualizare
+      {/if}
+    </div>
+    <div class="col-start-6  row-start-1 my-auto text-center">Profesor Responsabil</div>
+    <div
+      class="col-start-2 col-span-5 row-start-2 row-span-full grid grid-cols-1 grid-rows-5 gap-y-sm ">
+      {#each works as lucrare}
+        <LucrariModel
+          type={lucrare.type}
+          teacher={lucrare.teacher}
+          time={lucrare.time}
+          subiect={lucrare.subject} />
+      {/each}
+    </div>
+  </div></LayoutContext>
