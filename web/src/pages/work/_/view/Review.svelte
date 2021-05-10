360bb0e85bce42ba1014b210b935f083adf313a7
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 01:10:52 +0000

    Enable reviewing only if the work is in review

--- web/src/pages/work/_/view/Review.svelte
+++ web/src/pages/work/_/view/Review.svelte
@@ -1,7 +1,8 @@
 <script context="module" lang="ts">
-  import { go, Form, LayoutContext } from '$/components'
+  import { go, Form, LayoutContext, Link } from '$/components'
   import type { SubmitArgs } from '$/components'
-  import { fromMutation } from '$/lib'
+  import { fromMutation, status } from '$/lib'
+  import type { UserStatus } from '$/lib'
   import client from '$/graphql/client'
   import { UPDATE_WORK_STATUS } from '$/graphql/queries'
   import type { WorkStatus } from '$/graphql/queries'
@@ -30,7 +31,11 @@
   import type { UnrevisedWork } from '..'
   import Base from './Base.svelte'
   import Input from './review/InputOptions.svelte'
-  import { goto } from '@roxi/routify'
+  import { goto, params } from '@roxi/routify'
+
+  let user: UserStatus | undefined
+
+  status().then(s => (user = s))
 
   export let work: UnrevisedWork
 </script>
@@ -38,16 +43,25 @@
 {#await work.data then { workID }}
   <LayoutContext let:alive>
     <Base {work} additionalHeadingText="de {work.user}">
-      <Form
-        name="review"
-        cols={2}
-        rows={1}
-        hasTitle={false}
-        message="Lucrare revizuită cu succes!"
-        onSubmit={args => onSubmit(workID, alive, args, $goto)}
-        submitOnChange>
-        <Input />
-      </Form>
+      <span slot="heading">
+        {#if $params.back}
+          <Link href={$params.back} title="Întoarce-te de unde ai venit">
+            <span class="font-sans text-sm antialiased">Înapoi</span>
+          </Link>
+        {/if}
+      </span>
+      {#if work.status === 'inReview' && work.teacherID === user?.id}
+        <Form
+          name="review"
+          cols={2}
+          rows={1}
+          hasTitle={false}
+          message="Lucrare revizuită cu succes!"
+          onSubmit={args => onSubmit(workID, alive, args, $goto)}
+          submitOnChange>
+          <Input />
+        </Form>
+      {/if}
     </Base>
   </LayoutContext>
 {/await}
