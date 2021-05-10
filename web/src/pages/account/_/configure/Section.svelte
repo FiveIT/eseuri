ca7c55f883aadb5600e57fff26c7f964deda0abb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 22:39:22 +0000

    Extract config section into separate components

---
+++ web/src/pages/account/_/configure/Section.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { filterShadow, text } from '$/lib'
+  import { LayoutContext } from '$/components'
+</script>
+
+<LayoutContext let:theme>
+  <section
+    class="row-span-3 col-span-3 grid grid-cols-1 auto-rows-layout gap-y-sm font-sans text-sm {text[
+      theme
+    ]} {filterShadow[theme]}">
+    <slot />
+  </section>
+</LayoutContext>
