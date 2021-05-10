aa9742bf070d31c11cd0628e65577f7b4b7227ad
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 23:49:19 +0000

    Implement account configure page

--- web/src/pages/account/configure.svelte
+++ web/src/pages/account/configure.svelte
@@ -3,21 +3,55 @@
   import Error from './_/configure/Error.svelte'
   import Form from './_/configure/Form.svelte'
   import Spinner from './_/configure/Spinner.svelte'
+  import Section from './_/configure/Section.svelte'
+  import Header from './_/configure/Header.svelte'
+  import TeacherRequest from './_/configure/TeacherRequest.svelte'
+  import DeleteAccountModal from './_/configure/DeleteAccountModal.svelte'
+
+  import { border } from '$/lib'
+  import { LayoutContext } from '$/components'
+
+  import { Auth0LogoutButton } from '@tmaxmax/svelte-auth0'
+  import Modal, { openModal } from '@tmaxmax/renderless-svelte/src/Modal.svelte'
 
   const formData = self()
-
 </script>
 
-<section class="row-span-3 col-span-3 bg-white" />
-<section class="row-span-2 row-start-4 col-span-3 bg-white" />
-{#if $formData === null}
-  <Error>Nu am putut obține datele de utilizator, revino mai târziu.</Error>
-{:else if $formData}
-  {#if $formData.found}
-    <Form user={$formData.user} />
+<LayoutContext let:theme>
+  <div class="row-span-5 col-span-3 space-y-sm">
+    <Section>
+      <Header>Acțiuni cont</Header>
+      <TeacherRequest />
+      <Auth0LogoutButton
+        class="font-sans text-sm rounded {border.color[theme]} {border.all[theme]}">
+        Deconectează-te de la contul curent
+      </Auth0LogoutButton>
+    </Section>
+    <Section>
+      <Header>Zona periculoasă</Header>
+      <button
+        on:click={() => openModal(true)}
+        class="rounded {border.color[theme]} {border.all[
+          theme
+        ]} hover:(bg-red border-red) transition-all duration-100 ease-out font-sans text-sm antialiased">
+        Șterge-ți contul
+      </button>
+    </Section>
+  </div>
+  {#if $formData === null}
+    <Error>Nu am putut obține datele de utilizator, revino mai târziu.</Error>
+  {:else if $formData}
+    {#if $formData.found}
+      <Form user={$formData.user} />
+    {:else}
+      <Error>Ceva ciudat s-a întâmplat, reîncarcă pagina și vezi dacă merge!</Error>
+    {/if}
   {:else}
-    <Error>Ceva ciudat s-a întâmplat, reîncarcă pagina și vezi dacă merge!</Error>
+    <Spinner />
   {/if}
-{:else}
-  <Spinner />
-{/if}
+  <Modal let:payload>
+    {#if payload}
+      <DeleteAccountModal />
+    {/if}
+  </Modal>
+</LayoutContext>
