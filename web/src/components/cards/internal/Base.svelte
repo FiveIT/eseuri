ca178c5ea076c00c4ff9eff020c9df540e26166c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 10:08:20 +0000

    Fix overlay position

--- web/src/components/cards/internal/Base.svelte
+++ web/src/components/cards/internal/Base.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { fitText, BaseLayout } from '.'
+  import { px } from '$/lib'
+  import type { Theme } from '$/lib'
 
+  export let theme: Theme | undefined = undefined
+  export let borderColor: string | undefined = undefined
+  export let darkBg = false
   export let showOverlay = false
 </script>
 
-<BaseLayout>
+<BaseLayout {theme} {borderColor} {darkBg}>
   <dl class="grid w-full grid-flow-row h-full grid-rows-4 gap-y-xs">
-    <dt class="row-span-2 h-full flex flex-col" use:fitText={{ compensation: 1.1 }}>
+    <dt
+      class="row-span-2 h-full flex flex-col"
+      use:fitText={{ min: parseInt(px(1.5)), max: parseInt(px(1.75)) }}>
       <slot name="heading" />
     </dt>
     <dt class="self-center h-full flex flex-col" use:fitText>
@@ -17,12 +24,9 @@
     </dt>
     {#if showOverlay}
       <div
-        class="absolute w-full h-full duration-50 transition-opacity ease-out opacity-0 group-hover:opacity-100 group-focus:opacity-100">
+        class="absolute w-full h-full top-0 left-0 duration-50 transition-opacity ease-out opacity-0 group-hover:opacity-100 group-focus:opacity-100">
         <slot name="overlay" />
       </div>
     {/if}
   </dl>
 </BaseLayout>
-
-<style>
-</style>
