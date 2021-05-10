5119e2bc3a86e0c985a5ab339c54ab1de15c962e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 10:07:50 +0000

    Add background to button

--- web/src/components/DeleteButton.svelte
+++ web/src/components/DeleteButton.svelte
@@ -8,13 +8,13 @@
 <script lang="ts">
   import Icon from 'svelte-material-icons/Close.svelte'
 
-  import { px, border } from '$/lib'
+  import { px, border, background } from '$/lib'
   import { LayoutContext } from '$/components'
 </script>
 
 <LayoutContext let:theme>
   <button
-    class="rounded-full w-2em h-2em border-1 {border.color[
+    class="rounded-full w-2em h-2em border-1 {background[theme]} {border.color[
       theme
     ]} opacity-0 group-hover:opacity-100 focus:opacity-100 group-focus:opacity-100 flex items-center justify-center z-1"
     on:click>
