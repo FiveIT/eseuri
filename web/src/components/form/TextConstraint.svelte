c0b2f6fc73e249da5b28a87c6d663dff319e612a
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 16:30:15 +0000

    Create text form input with optional constraints

---
+++ web/src/components/form/TextConstraint.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Base from './internal/Base.svelte'
+  import { LayoutContext } from '$/components'
+  import { placeholderText, text } from '$/lib'
+
+  export let name: string
+  export let placeholder = ''
+  export let required = false
+  // eslint-disable-next-line no-undef
+  export let type: svelte.JSX.HTMLAttributes<HTMLInputElement>['type'] = 'text'
+  // eslint-disable-next-line no-unused-vars
+  export let check: (input: HTMLInputElement) => void = () => {}
+
+  let input: HTMLInputElement
+
+</script>
+
+<LayoutContext let:theme>
+  <Base>
+    <label for={name} class="place-self-center select-none text-center"><slot /></label>
+    <input
+      {type}
+      {name}
+      id={name}
+      {placeholder}
+      {required}
+      bind:this={input}
+      on:input={() => check(input)}
+      class="col-span-2 {text[theme]} placeholder-{placeholderText[theme].slice(
+        5
+      )} text-sm bg-transparent" />
+  </Base>
+</LayoutContext>
+
+<style>
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    transition: background-color 5000s ease-in-out 0s;
+  }
+
+</style>
