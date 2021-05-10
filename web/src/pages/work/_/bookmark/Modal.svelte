5c1f6a65adf6d7e0ee4a4a651b45e0d8657f026e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-05-10 17:46:17 +0000

    Use ModalBase

--- web/src/pages/work/_/bookmark/Modal.svelte
+++ web/src/pages/work/_/bookmark/Modal.svelte
@@ -3,22 +3,20 @@
     status: 'success',
     message: 'Marcaj creat cu succes, lucrarea este salvată!',
   } as const
+
 </script>
 
 <script lang="ts">
-  import { Form, Text, ActionsModal } from '$/components'
+  import { Form, Text, ActionsModal, ModalBase } from '$/components'
   import type { SubmitFn } from '$/components'
-  import { TRANSITION_EASING as easing } from '$/lib'
   import { closeModal } from '@tmaxmax/renderless-svelte/src/Modal.svelte'
 
-  import { fade } from 'svelte/transition'
   import { onMount } from 'svelte'
   import type { Work } from '..'
 
   export let work: Work
 
   let focus: () => void
-  let elem: HTMLElement
 
   const onSubmit: SubmitFn = ({ body }) =>
     work
@@ -27,25 +25,13 @@
       .then(() => notification)
 
   onMount(() => focus())
+
 </script>
 
-<div
-  class="bg fixed top-0 left-0 w-full h-full flex justify-center items-center z-100"
-  transition:fade={{ easing, duration: 50 }}>
-  <div
-    class="grid gap-x-md gap-y-sm px-md py-sm grid-cols-form auto-rows-layout bg-white rounded border-2px"
-    bind:this={elem}
-    on:blur={closeModal}>
-    <Form name="bookmark" {onSubmit} bind:focus cols={1} rows={2}>
-      <span slot="legend">Creează un marcaj</span>
-      <Text name="name" placeholder="Scrie numele marcajului aici..." required>Nume</Text>
-      <ActionsModal slot="actions" closeFn={closeModal}>Salvează lucrarea</ActionsModal>
-    </Form>
-  </div>
-</div>
-
-<style>
-  .bg {
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-</style>
+<ModalBase>
+  <Form name="bookmark" {onSubmit} bind:focus cols={1} rows={2}>
+    <span slot="legend">Creează un marcaj</span>
+    <Text name="name" placeholder="Scrie numele marcajului aici..." required>Nume</Text>
+    <ActionsModal slot="actions" closeFn={closeModal}>Salvează lucrarea</ActionsModal>
+  </Form>
+</ModalBase>
